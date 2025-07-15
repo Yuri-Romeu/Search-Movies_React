a65db550e4c4fabdbacf844f9e600fc6db3f4403
Yuri Romeu <yuriromeu2007@gmail.com>
2025-07-15 07:53:22 -0300

    Setup inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from './GlobalStyles';
+function App() {
+     return (
+          <>
+               <GlobalStyle />
+               <h1>Pagina react</h1>
+          </>
+     );
+}
+
+export default App;
